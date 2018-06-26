bc263c9d92f25b53c64c9bafaf39fabaf445119f
Mafe Avendaño <mafe.ud@gmail.com>
2018-06-26 09:50:21 -0500

    Crud generado mediante beego

---
+++ models/programa_academico.go
@@ -0,0 +1,153 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type ProgramaAcademico struct {
+	Id             int             `orm:"column(id);pk;auto"`
+	CodigoSnies    int             `orm:"column(codigo_snies)"`
+	Nombre         string          `orm:"column(nombre)"`
+	Institucion    int             `orm:"column(institucion)"`
+	Metodologia    *Metodologia    `orm:"column(metodologia);rel(fk)"`
+	NivelFormacion *NivelFormacion `orm:"column(nivel_formacion);rel(fk)"`
+	Titulacion     *Titulacion     `orm:"column(titulacion);rel(fk)"`
+	Duracion       float64         `orm:"column(duracion)"`
+	UnidadTiempo   int             `orm:"column(unidad_tiempo)"`
+}
+
+func (t *ProgramaAcademico) TableName() string {
+	return "programa_academico"
+}
+
+func init() {
+	orm.RegisterModel(new(ProgramaAcademico))
+}
+
+// AddProgramaAcademico insert a new ProgramaAcademico into database and returns
+// last inserted Id on success.
+func AddProgramaAcademico(m *ProgramaAcademico) (id int64, err error) {
+	o := orm.NewOrm()
+	id, err = o.Insert(m)
+	return
+}
+
+// GetProgramaAcademicoById retrieves ProgramaAcademico by Id. Returns error if
+// Id doesn't exist
+func GetProgramaAcademicoById(id int) (v *ProgramaAcademico, err error) {
+	o := orm.NewOrm()
+	v = &ProgramaAcademico{Id: id}
+	if err = o.Read(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+// GetAllProgramaAcademico retrieves all ProgramaAcademico matches certain condition. Returns empty list if
+// no records exist
+func GetAllProgramaAcademico(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(ProgramaAcademico))
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		qs = qs.Filter(k, v)
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
+	}
+
+	var l []ProgramaAcademico
+	qs = qs.OrderBy(sortFields...)
+	if _, err = qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
+}
+
+// UpdateProgramaAcademico updates ProgramaAcademico by Id and returns error if
+// the record to be updated doesn't exist
+func UpdateProgramaAcademicoById(m *ProgramaAcademico) (err error) {
+	o := orm.NewOrm()
+	v := ProgramaAcademico{Id: m.Id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Number of records updated in database:", num)
+		}
+	}
+	return
+}
+
+// DeleteProgramaAcademico deletes ProgramaAcademico by Id and returns error if
+// the record to be deleted doesn't exist
+func DeleteProgramaAcademico(id int) (err error) {
+	o := orm.NewOrm()
+	v := ProgramaAcademico{Id: id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Delete(&ProgramaAcademico{Id: id}); err == nil {
+			fmt.Println("Number of records deleted in database:", num)
+		}
+	}
+	return
+}
---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,170 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"],
+		beego.ControllerComments{
+			Method: "GetOne",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:MetodologiaController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"],
+		beego.ControllerComments{
+			Method: "GetOne",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:NivelFormacionController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"],
+		beego.ControllerComments{
+			Method: "GetOne",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:ProgramaAcademicoController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"],
+		beego.ControllerComments{
+			Method: "GetOne",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/programa_academico_crud/controllers:TitulacionController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
