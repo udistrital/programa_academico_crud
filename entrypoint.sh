482f3b96e4ea54c4ca1cb4d214d9ee88bfae2a23
Mafe Avendaño <mfavendannom@correo.udistrital.edu.co>
2021-02-22 07:01:24 -0500

    Ajustando planesticud a udistrital

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export PROGRAMA_ACADEMICO_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/programa_academico_crud/db/username --output text --query Parameter.Value)"
-  export PROGRAMA_ACADEMICO_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/programa_academico_crud/db/password --output text --query Parameter.Value)"
+  export PROGRAMA_ACADEMICO_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/campus/programa_academico_crud/db/username --output text --query Parameter.Value)"
+  export PROGRAMA_ACADEMICO_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/campus/programa_academico_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
