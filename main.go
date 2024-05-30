b3b05a4ad2da68058efd9766e2d22e5c3d9244cf
JulianAvella <julianavella01@gmail.com>
2024-05-30 11:21:51 -0500

    feat: Terminar la instrumentalización de las APIs udistrital/core_documentacion#29

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/planesticud/programa_academico_crud/routers"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/udistrital/utils_oas/customerror"
+	"github.com/udistrital/utils_oas/xray"
 )
 
 func init() {
@@ -38,7 +39,7 @@ func main() {
 	logPath += beego.AppConfig.String("logPath")
 	logPath += "\"}"
 	logs.SetLogger(logs.AdapterFile, logPath)*/
-
+	xray.InitXRay()
 	apistatus.Init()
 	//auditoria.InitMiddleware()
 	beego.ErrorController(&customerror.CustomErrorController{})
